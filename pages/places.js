0b2cd6dcfef41973e72351fc24b471cdf59866c7
Pankaj Gupta <pankajgupta@MacBook-Air.mshome.net>
2024-02-20 17:09:19 +0530

    update places page

--- pages/places.js
+++ pages/places.js
@@ -1,5 +1,18 @@
-function Places() {
-  return <h1>All Places</h1>;
+import PlaceList from '@/components/places/place-list';
+import { getPlaces } from '@/lib/db';
+
+function Places({ places }) {
+  return <PlaceList items={places} />;
 }
+
+export const getStaticProps = async () => {
+  const places = await getPlaces();
+
+  return {
+    props: {
+      places,
+    },
+  };
+};
 
 export default Places;
